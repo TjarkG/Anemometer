d499d7f844ef7ff006dd11459e9f140082623d9d
TjarkG <tjark.gaudich@outlook.de>
2021-11-24 18:45:15 +0100

    added UART Header and Schematic

--- main.c
+++ main.c
@@ -3,33 +3,43 @@
  *
  * Created: 22.11.2021 13:52
  * Author : tjark
- * Target : //TODO
+ * Target : Anemometer.sch
  */
 
-#ifndef __AVR_ATtiny2313__
-#define __AVR_ATtiny2313__
+#ifndef __AVR_ATtiny2313A__
+#define __AVR_ATtiny2313A__
 #endif
 
 #define F_CPU 8000000UL
 #define S_SENSOR 100        //Distance betwen Sensors in mm
 
+# define ECHO (PIND & 0x02)
+# define TRIG_PORT PORTD
+# define TRIG_PIN PIND3
+
 #include <avr/io.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 #include "HC_SR04.h"
+#include "uart.h"
 
 int main(void)
 {
-    long time = getSensorTime();
-    if(time > 0)
-    {
-        //v = d/t
-        long velocity = S_SENSOR/(time*1000);
-        uartWriteIntLine(velocity);
-    }
-    else
-    {
-        uartWriteString("Timeout");
-    }
+    DDRD = (1 << 3) | (1 << 1);
+    uartInit();
 
+    while(1)
+    {
+        long time = getSensorTime();
+        if(time > 0)
+        {
+            //v = d/t
+            long velocity = S_SENSOR/(time*1000);
+            uartWriteIntLine(velocity);
+        }
+        else
+        {
+            uartWriteString("Timeout\r\n");
+        }
+    }
 }
---
+++ uart.h
@@ -0,0 +1,76 @@
+#ifndef uart_H
+#define uart_H
+
+#include <avr/io.h>
+#include <string.h>
+
+#define BAUD 9600UL      // Baudrate
+
+#define UBRR_VAL ((F_CPU+BAUD*8)/(BAUD*16)-1)
+#define BAUD_REAL (F_CPU/(16*(UBRR_VAL+1)))
+#define BAUD_ERROR ((BAUD_REAL*1000)/BAUD)
+
+#if ((BAUD_ERROR<990) || (BAUD_ERROR>1010))
+  #error Systematischer Fehler der Baudrate grösser 1% 
+#endif
+
+/* reverse:  reverse string s in place */
+ void reverse(char s[])
+ {
+     int i, j;
+     char c;
+
+     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+         c = s[i];
+         s[i] = s[j];
+         s[j] = c;
+     }
+} 
+ 
+ /* itoa:  convert n to characters in s */
+ void _itoa(unsigned long n, char s[])
+ {
+     unsigned long i, sign;
+
+     if ((sign = n) < 0)  /* record sign */
+         n = -n;          /* make n positive */
+     i = 0;
+     do {       /* generate digits in reverse order */
+         s[i++] = n % 10 + '0';   /* get next digit */
+     } while ((n /= 10) > 0);     /* delete it */
+     if (sign < 0)
+         s[i++] = '-';
+     s[i] = '\0';
+     reverse(s);
+}
+
+void USART_Transmit( unsigned char data )       //Send one Char via UART
+{
+    while ( !( UCSRA & (1<<UDRE)) )
+        ;
+    UDR = data;
+}
+
+void uartWriteString(const char *in)
+{
+    while(*in)
+        USART_Transmit(*in++);
+}
+
+void uartWriteIntLine(unsigned long in)
+{
+    char tmp[12];
+    _itoa(in,tmp);
+    uartWriteString(tmp);
+    uartWriteString("\r\n");
+}
+
+void uartInit(void)
+{
+    UBRRH = UBRR_VAL >> 8;
+    UBRRL = UBRR_VAL & 0xFF;
+    UCSRB |= (1<<TXEN);  // UART TX einschalten
+    UCSRC = (1<<USBS) | (3<<UCSZ0);  // Asynchron 8N1 
+}
+
+#endif
